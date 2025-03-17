544499b695db5208925c5b321569c18a45ddcc83
Andrew Southern <whoamihealay@gmail.com>
2025-03-17 18:45:19 -0400

    added language server

--- after/plugin/mason.lua
+++ after/plugin/mason.lua
@@ -2,7 +2,7 @@ require('mason').setup({})
 require('mason-lspconfig').setup({
   -- Replace the language servers listed here
   -- with the ones you want to install
-  ensure_installed = {'eslint', 'lua_ls', 'rust_analyzer'},
+  ensure_installed = {'eslint', 'lua_ls', 'rust_analyzer', 'dockerls', 'ansiblels', 'helm_ls', 'jsonls'},
   handlers = {
     function(server_name)
       require('lspconfig')[server_name].setup({})

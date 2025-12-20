4a12b9ddf8a81d3aee2508690936a97dd1b0e63a
Andrew Southern <whoamihealay@gmail.com>
2025-12-20 18:11:17 -0500

    Added: fugitive, undotree

--- lua/whoamihealay/remap.lua
+++ lua/whoamihealay/remap.lua
@@ -2,3 +2,14 @@ vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.maplocalleader = "\\"
+
+vim.keymap.set("n", "=ap", "ma=ap'a")
+
+-- greatest remap ever
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+-- next greatest remap ever : asbjornHaland
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+vim.keymap.set({ "n", "v" }, "<leader>d", "\"_d")

e61e2126965df47dd2c96dc6f9c8ceb81bcd6081
Andrew Southern <whoamihealay@gmail.com>
2026-01-04 14:38:26 -0500

    Added: harpoon2

---
+++ lua/whoamihealay/lazy/harpoon.lua
@@ -0,0 +1,39 @@
+return {
+	"ThePrimeagen/harpoon",
+	branch = "harpoon2",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		local harpoon = require("harpoon")
+
+		harpoon:setup()
+
+		vim.keymap.set("n", "<C>A", function()
+			harpoon:list():prepend()
+		end)
+		vim.keymap.set("n", "<C>a", function()
+			harpoon:list():add()
+		end)
+		vim.keymap.set("n", "<C-e>", function()
+			harpoon.ui:toggle_quick_menu(harpoon:list())
+		end)
+
+		vim.keymap.set("n", "<C-1>", function()
+			harpoon:list():select(1)
+		end)
+		vim.keymap.set("n", "<C-2>", function()
+			harpoon:list():select(2)
+		end)
+		vim.keymap.set("n", "<C-3>", function()
+			harpoon:list():select(3)
+		end)
+		vim.keymap.set("n", "<C-4>", function()
+			harpoon:list():select(4)
+		end)
+		vim.keymap.set("n", "<M-n>", function()
+			harpoon:list():next()
+		end)
+		vim.keymap.set("n", "<M-p>", function()
+			harpoon:list():prev()
+		end)
+	end,
+}

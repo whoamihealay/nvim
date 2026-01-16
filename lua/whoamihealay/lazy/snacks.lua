adffd4b7e420763169afce5b2646416c1a802440
Andrew Southern <whoamihealay@gmail.com>
2026-01-16 15:51:25 -0500

    snacks tweaks

--- lua/whoamihealay/lazy/snacks.lua
+++ lua/whoamihealay/lazy/snacks.lua
@@ -5,8 +5,37 @@ return {
 		lazy = false,
 		---@type snacks.Config
 		opts = {
+			-- animate = { enabled = true },
+			-- bigfile = { enabled = true },
+			-- bufdelete = { enabled = true },
+			-- dashboard = { enabled = true },
+			-- debug = { enabled = true },
+			-- dim = { enabled = true },
+			explorer = { enabled = true, replace_netrw = false },
+			-- gh = { enabled = true },
+			git = { enabled = true },
+			-- gitbrowse = { enabled = true },
 			image = { enabled = true },
+			indent = { enabled = true },
+			input = { enabled = true },
+			-- keymap = { enabled = true },
+			-- layout = { enabled = true },
+			lazygit = { enabled = true },
+			-- notifier = { enabled = true },
+			-- notify = { enabled = true },
 			picker = { enabled = true },
+			-- profiler = { enabled = true },
+			-- quickfile = { enabled = true },
+			-- rename = { enabled = true },
+			scope = { enabled = true },
+			-- scroll = { enabled = true },
+			statuscolumn = { enabled = true },
+			-- terminal = { enabled = true },
+			-- toggle = { enabled = true },
+			-- util = { enabled = true },
+			-- win = { enabled = true },
+			words = { enabled = true },
+			-- zen = { enabled = true }
 		},
 		keys = {
 			-- Top Pickers & Explorer
@@ -75,6 +104,13 @@ return {
 				desc = "Recent",
 			},
 			-- git
+			{
+				"<leader>gg",
+				function()
+					Snacks.lazygit()
+				end,
+				desc = "Lazygit",
+			},
 			{
 				"<leader>gb",
 				function()

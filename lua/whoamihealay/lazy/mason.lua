f79df2c528bddc95c5d247df26c04e58a262d4e4
Andrew Southern <whoamihealay@gmail.com>
2026-01-22 21:44:33 -0500

    lualine and cleanup

--- lua/whoamihealay/lazy/mason.lua
+++ lua/whoamihealay/lazy/mason.lua
@@ -9,18 +9,30 @@ return {
 			"helm_ls",
 			"jsonls",
 			"lua_ls",
-			"stylua",
 			"tailwindcss",
 			"terraformls",
 			"tflint",
 			"vtsls",
+			"vimls",
 			"yamlls",
 		},
 	},
 	dependencies = {
 		{
 			"mason-org/mason.nvim",
+			build = ":MasonUpdate",
+			opts_extend = { "ensure_installed" },
 			opts = {
+				ensure_installed = {
+					"ansible-lint",
+					"hadolint",
+					"js-debug-adapter",
+					"markdownlint-cli2",
+					"markdown-toc",
+					"prettier",
+					"tflint",
+					"stylua",
+				},
 				ui = {
 					icons = {
 						package_installed = "✓",
@@ -53,6 +65,26 @@ return {
 					toggle_help = "g?",
 				},
 			},
+			config = function(_, opts)
+				require("mason").setup(opts)
+				local mr = require("mason-registry")
+				mr:on("package:install:sucess", function()
+					vim.defer_fn(function()
+						require("lazy.core.handler.event").trigger({
+							event = "FileType",
+							buf = vim.api.nvim_get_current_buf(),
+						})
+					end, 100)
+				end)
+				mr.refresh(function()
+					for _, tool in ipairs(opts.ensure_installed) do
+						local p = mr.get_package(tool)
+						if not p:is_installed() then
+							p:install()
+						end
+					end
+				end)
+			end,
 		},
 		"neovim/nvim-lspconfig",
 	},

cbb4927d7216923d108ea75e54941b2a07ac3fc8
Andrew Southern <whoamihealay@gmail.com>
2026-01-29 20:54:20 -0500

    fix lazydev

--- lua/whoamihealay/lazy/lazydev.lua
+++ lua/whoamihealay/lazy/lazydev.lua
@@ -3,11 +3,7 @@ return {
 	ft = "lua", -- only load on lua files
 	opts = {
 		library = {
-			-- See the configuration section for more details
-			-- Load luvit types when the `vim.uv` word is found
-			{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
 			{ path = "snacks.nvim", words = { "Snacks" } },
-			{ path = "lazy.nvim", words = { "LazyVim" } },
 		},
 	},
 }
